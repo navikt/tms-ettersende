e5156a1b8f1900da175330e292c00d3f0b6e062f
eivinne <eivinn_e@hotmail.com>
2022-03-09 15:36:45 +0100

    Endret basePath i server.js da den var satt opp feil tidligere.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const getHtmlWithDecorator = require("./dekorator");
-const basePath = "/ettersending";
+const basePath = "/tms-ettersendelse-fallback-vitejs";
 const buildPath = path.resolve(__dirname, "../");
 const server = express();
 
