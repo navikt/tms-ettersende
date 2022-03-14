a5f96ce2ba6f03444995c20761d8b05187774fcc
eivinne <eivinn_e@hotmail.com>
2022-03-14 17:16:44 +0100

    Forslag til utseende på guidepanel, pusher ut for test

--- src/components/lenkepanel/Lenkepanel.jsx
+++ src/components/lenkepanel/Lenkepanel.jsx
@@ -8,21 +8,23 @@ const Lenkepanel = () => {
   const translate = useIntl();
 
   return (
-    <Panel className="lenkepanel">
-      <Heading spacing size="small" className="header">
-        Flere muligheter
-      </Heading>
-      {lenkeliste.map((lenke) => (
-        <div className="lenke-wrapper" key={lenke.tittel}>
-          <Link className="lenke" href={lenke.url}>
-            {lenke.tittel}
-          </Link>
-          <BodyShort size="small" className="ingress">
-            {translate.formatMessage({ id: lenke.tekst, defaultMessage: "" })}
-          </BodyShort>
-        </div>
-      ))}
-    </Panel>
+    <div className="lenkepanel-wrapper">
+      <Panel className="lenkepanel">
+        <Heading spacing size="small" className="header">
+          Flere muligheter
+        </Heading>
+        {lenkeliste.map((lenke) => (
+          <div className="lenke-wrapper" key={lenke.tittel}>
+            <Link className="lenke" href={lenke.url}>
+              {lenke.tittel}
+            </Link>
+            <BodyShort size="small" className="ingress">
+              {translate.formatMessage({ id: lenke.tekst, defaultMessage: "" })}
+            </BodyShort>
+          </div>
+        ))}
+      </Panel>
+    </div>
   );
 };
 
--- src/components/mainComponent/MainComponent.jsx
+++ src/components/mainComponent/MainComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BodyShort, GuidePanel, Link, Heading } from "@navikt/ds-react";
+import { BodyShort, GuidePanel, Link, Heading, Panel } from "@navikt/ds-react";
 import Lenkepanel from "../lenkepanel/Lenkepanel";
 import { dittNavVarslingerUrl, soknaderEttersendelse, soknadOgSkjemaUrl } from "../../urls";
 import { useIntl } from "react-intl";
@@ -12,7 +12,7 @@ const MainComponent = () => {
       <Heading spacing size="xlarge" className="side-tittel">
         Ettersending
       </Heading>
-      <GuidePanel poster className="guide-panel">
+      <GuidePanel className="guide-panel">
         <BodyShort className="tekst-1">
           {translate.formatMessage({ id: "panel.tekst-1-del-1", defaultMessage: "" })}
           <Link href={dittNavVarslingerUrl}>
@@ -20,32 +20,36 @@ const MainComponent = () => {
           </Link>
           {translate.formatMessage({ id: "panel.tekst-1-del-2", defaultMessage: "" })}
         </BodyShort>
-        <BodyShort className="tekst-2">
-          {translate.formatMessage({ id: "panel.tekst-2-del-1", defaultMessage: "" })}
-          <Link href={soknaderEttersendelse}>
-            {translate.formatMessage({ id: "panel.tekst-2-lenke", defaultMessage: "" })}
-          </Link>
-        </BodyShort>
-        <ul className="steg-liste">
-          <li>
-            {translate.formatMessage({ id: "panel.steg-1-tekst", defaultMessage: "Gå til " })}
-            <Link href={soknadOgSkjemaUrl}>
-              {translate.formatMessage({ id: "panel.steg-1-lenke", defaultMessage: "søknad og skjema " })}
+      </GuidePanel>
+      <GuidePanel className="panel-2">
+        <div className="panel-2-tekst-wrapper">
+          <BodyShort className="tekst-2">
+            {translate.formatMessage({ id: "panel.tekst-2-del-1", defaultMessage: "" })}
+            <Link href={soknaderEttersendelse}>
+              {translate.formatMessage({ id: "panel.tekst-2-lenke", defaultMessage: "" })}
             </Link>
-          </li>
-          <li>
-            {translate.formatMessage({
-              id: "panel.steg-2-tekst",
-              defaultMessage: "Velg søknadskategori, deretter søknaden det gjelder",
-            })}
-          </li>
-          <li>
-            {translate.formatMessage({
-              id: "panel.steg-3-tekst",
-              defaultMessage: "Velg Ettersendelse (det kan være nødvendig å velge Ettersend på papir)",
-            })}
-          </li>
-        </ul>
+          </BodyShort>
+          <ul className="steg-liste">
+            <li>
+              {translate.formatMessage({ id: "panel.steg-1-tekst", defaultMessage: "Gå til " })}
+              <Link href={soknadOgSkjemaUrl}>
+                {translate.formatMessage({ id: "panel.steg-1-lenke", defaultMessage: "søknad og skjema " })}
+              </Link>
+            </li>
+            <li>
+              {translate.formatMessage({
+                id: "panel.steg-2-tekst",
+                defaultMessage: "Velg søknadskategori, deretter søknaden det gjelder",
+              })}
+            </li>
+            <li>
+              {translate.formatMessage({
+                id: "panel.steg-3-tekst",
+                defaultMessage: "Velg Ettersendelse (det kan være nødvendig å velge Ettersend på papir)",
+              })}
+            </li>
+          </ul>
+        </div>
       </GuidePanel>
       <Lenkepanel />
     </div>
